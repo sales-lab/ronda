52488db0de39f1bd0409508028a7d43b1281be0c
Gabriele Sales <gabriele.sales@unipd.it>
2024-11-21 19:02:38 +0100

    Retrieve package information with a separate function

--- R/packages.R
+++ R/packages.R
@@ -1,13 +1,28 @@
-#' Retrieve dependency information for all packages available on CRAN and on
-#' Bioconductor.
+#' Retrieve package information for all packages on CRAN and Bioconductor.
 #'
-#' @return A named list representing all dependencies of each package.
+#' @return A matrix, as returned by [available.packages()].
 #' @export
-all_package_deps <- function() {
+all_package_info <- function() {
   ap <- purrr::map(
     c(getOption("repos"), BiocManager::repositories()["BioCsoft"]),
     \(r) utils::available.packages(repos = r)
   )
-  ap <- do.call(rbind, ap)
-  tools::package_dependencies(rownames(ap), db = ap)
+  do.call(rbind, ap)
+}
+
+#' Retrieve dependency information for all packages available on CRAN and on
+#' Bioconductor.
+#'
+#' @param pkg_info Package information retrieved by [all_package_info()].
+#' @return A named list representing all dependencies of each package.
+#' @export
+all_package_deps <- function(pkg_info) {
+  deps <- tools::package_dependencies(rownames(pkg_info), db = pkg_info)
+  builtins <- unlist(tools::standard_package_names())
+  drop_builtins(deps, builtins)
+}
+
+drop_builtins <- function(deps, builtins) {
+  ns <- setdiff(names(deps), builtins)
+  purrr::map(deps[ns], \(ps) setdiff(ps, builtins))
 }
--- R/scheduler.R
+++ R/scheduler.R
@@ -1,20 +1,13 @@
 #' Computes a build schedule respecting inter-package dependencies.
 #'
-#' @param packages A list representing packages dependencies.
+#' @param deps A list representing packages dependencies.
 #' @return Vector of packages names in build order.
 #' @export
-build_schedule <- function(packages) {
-  builtins <- unlist(tools::standard_package_names())
-  deps <- drop_builtins(packages, builtins)
+build_schedule <- function(deps) {
   mapping <- intern_package_names(names(deps))
   ideps <- intern_dependencies(deps, mapping)
   ts <- toposort::topological_sort(ideps, dependency_type = "follows")
   names(mapping[ts])
-}
-
-drop_builtins <- function(deps, builtins) {
-  ns <- setdiff(names(deps), builtins)
-  purrr::map(deps[ns], \(ps) setdiff(ps, builtins))
 }
 
 intern_package_names <- function(packages) {

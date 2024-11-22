0d6bee4ade1685f3d8d1bb6969143b70ac21ba6a
Gabriele Sales <gabriele.sales@unipd.it>
2024-11-22 13:52:04 +0100

    Progress messages

--- R/demo.R
+++ R/demo.R
@@ -1,6 +1,7 @@
 #' Run a demonstration of the system, building the graphite package and its
 #' dependencies.
 #'
+#' @importFrom cli cli_progress_step
 #' @export
 demo_build <- function() {
   pis <- all_package_info() |> subset("graphite")
@@ -13,6 +14,10 @@ demo_build <- function() {
     }
 
     for (p in pkgs) {
+      cli_progress_step(
+        paste0("Building ", p),
+        msg_done = paste0("Built ", p)
+      )
       conda_build(p, pis)
     }
 

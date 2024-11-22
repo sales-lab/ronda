d1d77873361b83121ddb2ec724d8027d7addc139
Gabriele Sales <gabriele.sales@unipd.it>
2024-11-22 09:48:03 +0100

    Build schedule based on dependency counts

--- R/scheduler.R
+++ R/scheduler.R
@@ -1,21 +1,57 @@
 #' Computes a build schedule respecting inter-package dependencies.
 #'
-#' @param deps A list representing packages dependencies.
-#' @return Vector of packages names in build order.
+#' @param pkg_info Packages to build.
+#' @return A `build_sched` object.
 #' @export
-build_schedule <- function(deps) {
-  mapping <- intern_package_names(names(deps))
-  ideps <- intern_dependencies(deps, mapping)
-  ts <- toposort::topological_sort(ideps, dependency_type = "follows")
-  names(mapping[ts])
+build_schedule <- function(pkg_info) {
+  pkgs <- names(pkg_info)
+
+  counts <- rep.int(0, length(pkgs))
+  names(counts) <- pkgs
+
+  rdeps <- list()
+
+  for (p in pkgs) {
+    ds <- pkg_deps(pkg_info, p)
+
+    counts[p] <- length(ds)
+    for (d in ds) {
+      rdeps[[d]] <- c(rdeps[[d]], p)
+    }
+  }
+
+  structure(list(counts = counts, rdeps = rdeps), class = "build_sched")
 }
 
-intern_package_names <- function(packages) {
-  mapping <- seq_along(packages)
-  names(mapping) <- packages
-  mapping
+#' Print method for `build_sched` objects.
+#'
+#' @param x A `build_sched` object.
+#' @param ... Other arguments, ignored.
+#' @export
+print.build_sched <- function(x, ...) {
+  cat("Build schedule for", length(x$counts), "packages.\n")
 }
 
-intern_dependencies <- function(deps, mapping) {
-  purrr::map(deps, \(ns) purrr::keep(mapping[ns], \(x) !is.na(x)))
+#' List packages that can be build now.
+#'
+#' @param build_sched A build schedule.
+#' @return A vector of package names.
+buildable_pkgs <- function(build_sched) {
+  cs <- build_sched$counts
+  names(cs)[cs == 0]
+}
+
+#' Set packages as built.
+#'
+#' @param build_sched A build schedule.
+#' @param pkgs A vector of package names.
+#' @return An updated version of the build schedule.
+set_built_pkgs <- function(build_sched, pkgs) {
+  cs <- build_sched$counts
+  for (p in pkgs) {
+    rds <- build_sched$rdeps[[p]]
+    cs[rds] <- cs[rds] - 1
+  }
+
+  structure(list(counts = cs, rdeps = build_sched$rdeps), class = "build_sched")
 }
